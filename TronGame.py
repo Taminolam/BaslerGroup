fdde6e960b4ef6d9acc65ff1a3f0082b3680af26
Tomist2104 <Tom@stoelken.de>
2017-10-27 13:59:09 +0200

    endversinon

--- TronGame.py
+++ TronGame.py
@@ -170,7 +170,7 @@ def  handleCollisionOfLightCyclesAndTrails(distanceLimitPx, lightCycles, trails)
     for trail in trails:
         collisions = trail.getCollidedObjects(lightCycles, distanceLimitPx)
         if len(collisions) > 0:
-            for collision in collisions:
+            for collision in collisions:    
                 lightCycles[collision[0]._carId].destroy()
                 
 def handleCollisonOfBall (cars,ball):
@@ -184,6 +184,7 @@ def handleCollisonOfBall (cars,ball):
     if (ball._position.y >380 and ball._position.y <820 and ball._position.x <320 and ball._position.x >0) or (ball._position.y >380 and ball._position.y <820 and ball._position.x >1480 and ball._position.x <1800): 
         ball._position = BAPI.Point (900,600)      
         ball.speed = 0    
+
 def handleCollisionOfLightCyclesWithEachOther(distanceLimitPx, lightCycles):
     # collision lightCycles with each other
     if calcDistance(lightCycles[0].getPosition(), lightCycles[1].getPosition()) < distanceLimitPx:
